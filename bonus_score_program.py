c97bf67e3d88838ad0faaec75498ec135e7ac7a3
abhinav-coding <nobodyknows7994@gmail.com>
2025-06-30 12:34:36 +0530

    Add files via upload

---
+++ bonus_score_program.py
@@ -0,0 +1,9 @@
+
+line = "-" * 45
+name = input("YOUR NAME?\n")
+score = int(input("WHAT IS YOUR CURRENT SCORE?"))
+total_score = score + 50
+
+print(line)
+print(f"\nCongratulations! {name}.\n\nYou are the winner of the game and we are giving you 50 bonus points so your current points have increased to {total_score} points. We appreciate your progress.\n")
+print(line)
